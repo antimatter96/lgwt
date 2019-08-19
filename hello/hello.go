5fc84d9e8f33a9102722a52fde08534434edfbfe
Arpit Jain <antimatter96@users.noreply.github.com>
2019-08-19 19:58:37 +0530

    4

--- hello/hello.go
+++ hello/hello.go
@@ -1,9 +1,9 @@
 package main
 
 import "fmt"
-
+const englishHelloPrefix = "Hello, "
 func Hello(name string)string {
-    return fmt.Sprintf("Hello, %s", name)
+    return englishHelloPrefix + name
 }
 
 func main() {

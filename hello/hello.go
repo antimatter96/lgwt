5b10f06ed801abe636568344bc5e424bc8c7753d
Arpit Jain <antimatter96@users.noreply.github.com>
2019-08-20 22:02:47 +0530

    5

--- hello/hello.go
+++ hello/hello.go
@@ -1,11 +1,16 @@
 package main
 
 import "fmt"
+
 const englishHelloPrefix = "Hello, "
-func Hello(name string)string {
-    return englishHelloPrefix + name
+
+func Hello(name string) string {
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
 }
 
 func main() {
-    fmt.Println(Hello("World"))
+	fmt.Println(Hello("World"))
 }

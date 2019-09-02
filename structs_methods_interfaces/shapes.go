4982ff926c429f57382609b957aaaf1d6d27db88
Arpit Jain <antimatter96@users.noreply.github.com>
2019-09-03 01:43:11 +0530

    Add circle. Will faild

--- structs_methods_interfaces/shapes.go
+++ structs_methods_interfaces/shapes.go
@@ -1,16 +1,29 @@
 package shapes
 
+import (
+	"math"
+)
+
 type Rectangle struct {
-	length  float64
-	breadth float64
+	Length  float64
+	Breadth float64
+}
+
+type Circle struct {
+	Radius float64
 }
 
 func Perimeter(rectangle Rectangle) (perimeter float64) {
-	perimeter = 2 * (rectangle.length + rectangle.breadth)
+	perimeter = 2 * (rectangle.Length + rectangle.Breadth)
 	return
 }
 
 func Area(rectangle Rectangle) (perimeter float64) {
-	perimeter = rectangle.length * rectangle.breadth
+	perimeter = rectangle.Length * rectangle.Breadth
+	return
+}
+
+func Area(circle Circle) (perimeter float64) {
+	perimeter = circle.Radius * circle.Radius * math.Pi
 	return
 }

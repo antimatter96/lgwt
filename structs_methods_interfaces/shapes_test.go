011975006d497b2ee0aa32e00c925aff749cc016
Arpit Jain <antimatter96@users.noreply.github.com>
2019-09-03 02:17:55 +0530

    Refactor

--- structs_methods_interfaces/shapes_test.go
+++ structs_methods_interfaces/shapes_test.go
@@ -5,19 +5,23 @@ import "testing"
 func TestPerimeter(t *testing.T) {
 
 	perimeterTests := []struct {
+		name  string
 		shape Shape
 		want  float64
 	}{
-		{Rectangle{10.0, 10.0}, 40.0},
-		{Circle{10.0}, 62.831853071795862},
-		{Triangle{3.0, 5.0, 6.0}, 14.0},
+		{name: "rectangle", shape: Rectangle{10.0, 10.0}, want: 40.0},
+		{name: "circle", shape: Circle{10.0}, want: 62.831853071795862},
+		{name: "triangle", shape: Triangle{3.0, 5.0, 6.0}, want: 14.0},
 	}
 
 	for _, tt := range perimeterTests {
-		got := tt.shape.Perimeter()
-		if got != tt.want {
-			t.Errorf("got %.2f want %.2f", got, tt.want)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Perimeter()
+			if got != tt.want {
+				t.Errorf("%#v got %.2f want %.2f", tt.shape, got, tt.want)
+			}
+		})
+
 	}
 
 }
@@ -25,19 +29,22 @@ func TestPerimeter(t *testing.T) {
 func TestArea(t *testing.T) {
 
 	areaTests := []struct {
+		name  string
 		shape Shape
 		want  float64
 	}{
-		{Rectangle{10.0, 9.0}, 90.0},
-		{Circle{10}, 314.1592653589793},
-		{Triangle{3.0, 5.0, 6.0}, 7.483314773547883},
+		{name: "rectangle", shape: Rectangle{10.0, 9.0}, want: 90.0},
+		{name: "circle", shape: Circle{10}, want: 314.1592653589793},
+		{name: "triangle", shape: Triangle{3.0, 5.0, 6.0}, want: 7.483314773547883},
 	}
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("got %.2f want %.2f", got, tt.want)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.want {
+				t.Errorf("%#v got %.2f want %.2f", tt.shape, got, tt.want)
+			}
+		})
 	}
 
 }

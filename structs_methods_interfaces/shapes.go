cd4f9f9e0f6e9f0110dd35ee26b5c80d0b04847f
Arpit Jain <antimatter96@users.noreply.github.com>
2019-09-03 01:37:35 +0530

    Change to struct

--- structs_methods_interfaces/shapes.go
+++ structs_methods_interfaces/shapes.go
@@ -1,11 +1,16 @@
 package shapes
 
-func Perimeter(l, b float64) (perimeter float64) {
-	perimeter = 2 * (l + b)
+type Rectangle struct {
+	length  float64
+	breadth float64
+}
+
+func Perimeter(rectangle Rectangle) (perimeter float64) {
+	perimeter = 2 * (rectangle.length + rectangle.breadth)
 	return
 }
 
-func Area(l, b float64) (perimeter float64) {
-	perimeter = l * b
+func Area(rectangle Rectangle) (perimeter float64) {
+	perimeter = rectangle.length * rectangle.breadth
 	return
 }

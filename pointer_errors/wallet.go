76e84388adb0e0ce60d0dee59cbbfe7b01fea29e
Arpit Jain <antimatter96@users.noreply.github.com>
2019-09-23 19:19:30 +0530

    Pass tests

--- pointer_errors/wallet.go
+++ pointer_errors/wallet.go
@@ -1,11 +1,14 @@
 package pointer_errors
 
-type Wallet struct{}
-
-func (w Wallet) Deposit(amount int) {
-
+type Wallet struct {
+	balance int
 }
 
-func (w Wallet) Balance() int {
-	return 0
+//struct pointers
+func (w *Wallet) Deposit(amount int) {
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() int {
+	return w.balance
 }

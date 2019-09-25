70eea84c344252767885b595c24bdd4367316630
Arpit Jain <antimatter96@users.noreply.github.com>
2019-09-25 21:14:46 +0530

    Error check and tests

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,7 +1,17 @@
 package dictionary
 
+import (
+	"errors"
+)
+
 type Dictionary map[string]string
 
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
 func (d Dictionary) Search(word string) (string, error) {
-	return d[word], nil
+	def, found := d[word]
+	if !found {
+		return "", ErrNotFound
+	}
+	return def, nil
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -8,21 +8,17 @@ func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
 	t.Run("known word", func(t *testing.T) {
-		got, _ := dictionary.Search("test")
+		got, err := dictionary.Search("test")
 		want := "this is just a test"
 
 		assertString(t, got, want)
+		assertNoError(t, err)
 	})
 
 	t.Run("unknown word", func(t *testing.T) {
 		_, err := dictionary.Search("unknown")
-		want := "could not find the word you were looking for"
 
-		if err == nil {
-			t.Fatal("expected to get an error.")
-		}
-
-		assertString(t, err.Error(), want)
+		assertError(t, err, ErrNotFound)
 	})
 }
 
@@ -30,5 +26,24 @@ func assertString(t *testing.T, got, want string) {
 	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func assertError(t *testing.T, got, want error) {
+	t.Helper()
+
+	if got == nil {
+		// Mo further useful info in this test, so fatal
+		t.Fatalf("didn't get an error but wanted one")
+	}
+	if got != want {
+		t.Errorf("got error %q want %q", got, want)
+	}
+}
+
+func assertNoError(t *testing.T, got error) {
+	t.Helper()
+	if got != nil {
+		t.Errorf("got an error but didn't want one")
 	}
 }

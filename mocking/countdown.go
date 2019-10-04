2bd373fda6d81517bb8d62c2eba123e93aed4cf9
Arpit Jain <antimatter96@users.noreply.github.com>
2019-10-04 19:48:32 +0530

    Pass the tests

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -12,10 +12,10 @@ const countdownStart = 3
 
 func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep()
 		fmt.Fprintf(out, "%d\n", i)
 	}
-	time.Sleep(1 * time.Second)
+	sleeper.Sleep()
 	fmt.Fprintf(out, "%s", finalWord)
 }
 

0a8eb56fc35d672afee0cb86c8662ea06dc29bbd
Arpit Jain <antimatter96@users.noreply.github.com>
2019-10-04 20:07:10 +0530

    Add mock for both write and sleep. Add tests to test order of sleep/write. Fix func to original slef

--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
@@ -13,23 +14,58 @@ func (s *SpySleeper) Sleep() {
 	s.Calls++
 }
 
+const writeOP = "write"
+const sleepOP = "sleep"
+
+type SuperSpy struct {
+	Calls []string
+}
+
+func (ss *SuperSpy) Write(p []byte) (int, error) {
+	ss.Calls = append(ss.Calls, writeOP)
+	return 0, nil
+}
+
+func (ss *SuperSpy) Sleep() {
+	ss.Calls = append(ss.Calls, sleepOP)
+
+}
+
 func TestCountdown(t *testing.T) {
-	buffer := &bytes.Buffer{}
-	spySleeper := &SpySleeper{}
+	t.Run("Prints 3,2,1,Go!", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		superSpy := &SuperSpy{}
 
-	Countdown(buffer, spySleeper)
+		Countdown(buffer, superSpy)
 
-	got := buffer.String()
-	want := `3
+		got := buffer.String()
+		want := `3
 2
 1
 Go!`
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 
-	if spySleeper.Calls != 4 {
-		t.Errorf("not enough calls to sleeper, want 4 got %d", spySleeper.Calls)
-	}
+	})
+
+	t.Run("Sleeps before every write", func(t *testing.T) {
+
+		superSpy := &SuperSpy{}
+
+		Countdown(superSpy, superSpy)
+
+		want := []string{
+			"sleep", "write",
+			"sleep", "write",
+			"sleep", "write",
+			"sleep", "write",
+		}
+		if !reflect.DeepEqual(want, superSpy.Calls) {
+			t.Errorf("got %q want %q", superSpy.Calls, want)
+		}
+
+	})
+
 }

298509eea2f3870bfe568bf6b923b120b579ff24
Arpit Jain <antimatter96@users.noreply.github.com>
2019-10-22 15:36:53 +0530

    Add passing code

--- racer/racer.go
+++ racer/racer.go
@@ -1,5 +1,22 @@
 package racer
 
-func Racer(url1, url2 string) (winner string) {
-	return
+import (
+	"net/http"
+	"time"
+)
+
+func Racer(url1, url2 string) string {
+	start1 := time.Now()
+	http.Get(url1)
+	duration1 := time.Since(start1)
+
+	start2 := time.Now()
+	http.Get(url2)
+	duration2 := time.Since(start2)
+
+	if duration1 < duration2 {
+		return url1
+	}
+
+	return url2
 }
